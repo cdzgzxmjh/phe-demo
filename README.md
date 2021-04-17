2e8eb4ae50474fcd49c56b7794c97e4e6b7fab4d
maijiaheng <maijiaheng>
2021-04-17 17:06:36 +0800

    补充注释

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## 一个简单的PHE（部分同态加密）例子
 同态加密（基于PHE）的一个简单例子，模拟客户端与云端通信  
 PHE算法取自 data61/python-paillier 项目，url: https://github.com/data61/python-paillier
+此同态算法支持：
+1. 加密数据相加
+2. 加密数据与非加密数据相加
+3. 加密数据与非加密数据相乘
+
+例子为加密数据相加
--- demo/example.py
+++ demo/example.py
@@ -15,45 +15,59 @@ class ExampleEncodedNumber(phe.encoding.EncodedNumber):
     LOG2_BASE = math.log(BASE, 2)
 
 
+# 模拟客户端
 class Client:
+    # 必须定义密钥对
     def __init__(self, pub_key, pri_key, encrypted_x=None, encrypted_y=None):
         self.pub_key = pub_key
         self.pri_key = pri_key
         self.encrypted_x = encrypted_x
         self.encrypted_y = encrypted_y
 
+    # 加密
     def encrypt(self, x, y):
+        # 对加密数据进行编码包装，标识加密数据
         encoded_x = ExampleEncodedNumber.encode(self.pub_key, x)
         encoded_y = ExampleEncodedNumber.encode(self.pub_key, y)
+        # 执行加密
         self.encrypted_x = self.pub_key.encrypt(encoded_x)
         self.encrypted_y = self.pub_key.encrypt(encoded_y)
 
+    # 解密
+    def decrypt(self, encrypted):
+        return self.pri_key.decrypt_encoded(encrypted, ExampleEncodedNumber).decode()
+
+    # 评估函数
     @classmethod
     def evaluate(cls, x, y):
         return x + y
 
+    # 目标计算函数
     @classmethod
     def expect(cls, x, y):
         return x + y
 
-    def decrypt(self, encrypted):
-        return self.pri_key.decrypt_encoded(encrypted, ExampleEncodedNumber).decode()
 
-
+# 模拟云服务
 class Cloud:
+    # 必须定义公钥与评估函数
     def __init__(self, pub_key, evaluate):
         self.pub_key = pub_key
         self.evaluate = evaluate
 
+    # 模拟云服务计算
     def calculate(self, x, y):
         return self.evaluate(x, y)
 
 
 def math_example(pub_key, pri_key, a, b, h=None):
     client = Client(pub_key, pri_key)
+    # 客户端加密
     client.encrypt(a, b)
     cloud = Cloud(pub_key, Client.evaluate)
+    # 云服务计算
     encrypted = cloud.calculate(client.encrypted_x, client.encrypted_y)
+    # 客户端解密
     result = client.decrypt(encrypted)
     assert abs(Client.expect(p1, p2) - result) < 1e-15
     print("成功")
